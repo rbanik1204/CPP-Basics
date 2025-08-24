8702a771b65aedc66e4cfe5138dee0b3f5e70a3b
rb1204 <ratulbanik2004@gmail.com>
2025-08-25 02:10:05 +0530

    Initial Commit

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string
+// int main(){
+//     const int a =10;
+//     cout<<a<<endl;
+//     return 0;
+// }
+int main(){
+    string name ="rbanik";
+    cout << "Hello, " << name << "!" <<endl;
+    return 0;
+}
