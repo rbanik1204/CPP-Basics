7f61f904a1f82896265a14d61a6ac22ee25c8685
rb1204 <ratulbanik2004@gmail.com>
2026-03-22 23:57:25 +0530

    cin object & getline free function

--- cppstrings_input.cpp
+++ cppstrings_input.cpp
@@ -10,8 +10,13 @@ int main(void){
     string s2;
     getline(cin,s2,',');// getline(cin,base_string <char>,char delim)
     //getline reads entire line including tabs and spaces . '\n' is the only string terminator 
-    //Type 3 -------------mixing cin & getline---------
+    //Type 3 -------------mixing cin & getline----------
     string s3;
     int x;
+    cin>>x;
+    //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    cin.ignore();//Flush leftover '\n' from the input buffer
+    cin>>s3;
+    cout<<"Type 3: "+s3+" -> Mixed with cin & getline";
     return 0;
 }
--- cstring_lib_funcs.cpp
+++ cstring_lib_funcs.cpp
@@ -3,8 +3,10 @@
 // #include<cstd
 using namespace std;
 int main(void){
-    char *ch = (char*)malloc(10*sizeof(char));
-    cin.getline(ch,10);
+    char *ch = (char*)malloc(10*sizeof(char));//ch stored in stack and whole char []
+    cin.getline(ch,10,',');//dynamically manages the memory of the std::string object.(member function)
+    //cin.getline(char_array, size); This is a member function of input stream objects (like cin) used to read a stream of characters into a C-style character array (C string)
     cout<<ch;
+    //All the Cstring library functions are applicable
     return 0;
 }
